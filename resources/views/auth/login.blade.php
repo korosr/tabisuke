0f18eaf886c9f0775ea1e912ea8019808a6bf8e1
korosr <kaoriisa1206@gmail.com>
2021-03-22 00:31:44 +0900

    ログイン、ログアウト機能追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,9 +3,10 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('Login') }}<span class="text-le"><a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
